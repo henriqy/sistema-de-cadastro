0bccef8623e65e08d6af66dbb89c7817d5049c5c
henriqy <henrisoft3@gmail.com>
2023-02-23 00:04:27 -0300

    voltando para o estado anterior

--- app.js
+++ app.js
@@ -2,5 +2,3 @@ console.log("Rodando o sistema de gerenciamneto de jogos")
 console.log("aplicaçao esta se conectado ao banco de dados ")
 
 console.log("o paulo vez essa commit sem voce ver na aula")
-
-console.log("adicionando um console log")
