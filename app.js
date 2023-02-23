81ea7ce0994173b876e05c59e6d43e5301c365ed
Henrique Oliveira <henrisoft3@gmail.com>
2023-02-22 23:55:56 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o sistema de gerenciamneto de jogos")
 console.log("aplicaçao esta se conectado ao banco de dados ")
 
 console.log("o paulo vez essa commit sem voce ver na aula")
+
+console.log("adicionando um console log")
